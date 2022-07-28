669d70c9460f48dac3262aca2848ce543d39e309
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-07-28 16:25:06 +0530

    Update CountObject.java

--- oops/CountObject.java
+++ oops/CountObject.java
@@ -1,18 +1,19 @@
 package oops;
 
 public class CountObject {
-	static int count=0;
+	static int count=0;//static counter variable
 	CountObject()
 	{
-		count++;
+		count++;// increment counter
 		System.out.println(CountObject.count);
 		
 	}
 	
 	
 	
-
+	
 	public static void main(String[] args) {
+		//object creation
 		CountObject c1=new CountObject();
 		CountObject c2=new CountObject();
 		CountObject c3=new CountObject();
