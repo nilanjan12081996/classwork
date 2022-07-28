e2a8ad4dbbdfffcb5539328c9e959c10d2eba95c
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-07-28 16:28:43 +0530

    Update HirarachicalInheritance.java

--- oops/HirarachicalInheritance.java
+++ oops/HirarachicalInheritance.java
@@ -1,38 +1,42 @@
 package oops;
+//parent class
 class Bank{
 	public void loanInterest()
 	{
 		System.out.println("9% interest");
 	}
 }
+//Sbi is subclass which inherit parentclass Bank 
 class Sbi extends Bank
 {
-	public void sbiLoan() {
+	public void sbiLoan() {//sbiLoan Method
 		System.out.println("For SBI");
 	}
 	}
+	//Hdfc is subclass which inherit parentclass Bank 
 class Hdfc extends Bank
 {
-	public void hdfcLoan() {
+	public void hdfcLoan() {//HdfcLoan Method
 		System.out.println("For HDFC");
 	}
 	}
+	//Axis is subclass which inherit parentclass Bank 
 class Axis extends Bank
 {
-	public void axisLoan() {
+	public void axisLoan() {//axisLoan Method
 		System.out.println("For AXIS");
 	}
 	}
 public class HirarachicalInheritance {
 
 	public static void main(String[] args) {
-		Axis ax=new Axis();
+		Axis ax=new Axis(); //instantiate Axis class 
 		ax.axisLoan();
 		ax.loanInterest();
-		Hdfc hd=new Hdfc();
+		Hdfc hd=new Hdfc();//instantiate Hdfc class 
 		hd.hdfcLoan();
 		hd.loanInterest();
-		Sbi sb=new Sbi();
+		Sbi sb=new Sbi();//instantiate Sbi class
 		sb.sbiLoan();
 		sb.loanInterest();
 	}
