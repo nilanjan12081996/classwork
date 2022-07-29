6fee7ca7ac684d0d2bc66c53ccabbc6359f8f4fe
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-07-29 16:56:38 +0530

    Update MultipleInheritance.java

--- oops/interface/MultipleInheritance.java
+++ oops/interface/MultipleInheritance.java
@@ -1,18 +1,21 @@
+/*
+@author Nilanjan Dasgupta
+*/
 package abstractiondemo;
+//abstract class
 abstract class A
 {
-	abstract void display();
-	public void demo()
+	abstract void display();// abstract method
+	public void demo()// concrete method
 	{
 		System.out.println("abstract class");
 	}
 	}
-interface in
+interface in 	//interface in
 {
 	int add(int a,int b);
 	}
-class Child extends A implements in 
-{
+class Child extends A implements in //class child implements interface in and extends class A
 
 	@Override
 	public int add(int a, int b) {
