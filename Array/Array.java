4ba4d2f221513bbde4e20d1896a126e651cb6020
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-08-03 17:25:40 +0530

    Update Array.java

--- Array/Array.java
+++ Array/Array.java
@@ -1,3 +1,7 @@
+/*
+ * Author Nilanjan Dasgupta
+ *
+ */
 package array;
 
 import java.util.Scanner;
@@ -6,26 +10,25 @@ public class Array {
 
 	public static void main(String[] args) {
 		//statically input array
-		int arr[]= {4,5,6,8,7};
-		for(int i=0;i<arr.length;i++)
+		int arr[]= {4,5,6,8,7};//create array statically
+		for(int i=0;i<arr.length;i++)//for loop for printing the array elements
 		{
 			System.out.println(arr[i]);
 		}
 		//user input
 		Scanner sc=new Scanner(System.in);
-		int arr2[]=new int[10];
-		int sum=0;
+		int arr2[]=new int[10];//create a array
 		System.out.println("enter the number of elements");
-		int n=sc.nextInt();
+		int n=sc.nextInt();// take users from number of elemenets
 		for(int i =0;i<n;i++)
 		{
 			System.out.print("enter the elements: ");
-			arr2[i]=sc.nextInt();
+			arr2[i]=sc.nextInt();// take input from user for array elements
 			}
 			System.out.println("Elements are: ");
-			for(int i =0;i<n;i++)
+			for(int i =0;i<n;i++)//for loop for printing the array elements
 		{
-				System.out.println(arr2[i]);;
+				System.out.println(arr2[i]);
 			}
 	}
 
