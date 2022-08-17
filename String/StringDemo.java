c4c012e905c028dcd54360102fd26547f80711d7
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-08-17 17:40:01 +0530

    Create StringDemo.java

---
+++ String/StringDemo.java
@@ -0,0 +1,19 @@
+package string;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+	//	String name1="Nilanjan";
+		//String name2="Nilanjan";
+		//String name3="welcome";
+		//System.out.println(name1==name2);
+		//System.out.println(name1==name3);
+		// name1=name1.concat(" Dasgupta");
+		//System.out.println(name1);
+		String name="good morning";
+		//char ch=name.charAt(5);
+		char ch=name.charAt(-1);
+		System.out.println(ch);
+	}
+
+}
